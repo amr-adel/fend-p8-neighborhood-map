2075c6961ce9610a3ae62930f651c1105b60b80c
Amr A. Abdelmoez <amradel88@live.com>
2018-06-26 06:26:03 +0200

    feat: show/hide info when a place clicked

--- src/ListPlaces.js
+++ src/ListPlaces.js
@@ -2,7 +2,34 @@ import React, { Component } from 'react';
 
 class ListPlaces extends Component {
 
+    state = {
+
+    }
+
+    filter(query) {
+        this.props.filter(query)
+
+        this.hideInfo()
+    }
+
+    showInfo(mall) {
+        this.props.showInfo(mall)
+
+        this.hideInfo()
+
+        document.getElementById(mall.gmapId).classList.add('selected')
+    }
+
+    hideInfo() {
+        let selected = document.getElementsByClassName('selected')
+
+        if (selected.length !== 0) {
+            selected[0].classList.remove('selected')
+        }
+    }
+
     render() {
+
         return (
             <section className="sidebar">
                 <header className="header">
@@ -11,12 +38,13 @@ class ListPlaces extends Component {
                     </div>
                 </header>
 
-                <input type="text" className="filter" placeholder="Filter Places" onChange={e => this.props.filter(e.target.value)} />
+                <input type="text" className="filter" placeholder="Filter Places" onChange={e => this.filter(e.target.value)} />
 
                 <ol className="places">
                     {this.props.filteredMalls.map(mall => (
-                        <li className="place" key={mall.gmapId} onClick={() => this.props.showInfo(mall)} >
-                            {mall.name}
+                        <li className="place" key={mall.gmapId} id={mall.gmapId} onClick={() => this.showInfo(mall)} >
+                            <h4 className="name">{mall.name}</h4>
+                            <div className="details">Loading mall details...</div>
                         </li>
                     ))}
                 </ol>
