b91b295bf749b87f7aa1ec0e301dd58d2d8c2862
Amr A. Abdelmoez <amradel88@live.com>
2019-08-05 02:54:40 +0200

    Updates

---
+++ precache-manifest.a7b86236dc1467c3cf759df232310ed4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65cbfffd77c0dccb6223cea95954e64",
+    "url": "/malls-of-cairo/index.html"
+  },
+  {
+    "revision": "df0019377c993e5f79f9",
+    "url": "/malls-of-cairo/static/css/main.be92bb85.chunk.css"
+  },
+  {
+    "revision": "bf508d0733535fa5eb69",
+    "url": "/malls-of-cairo/static/js/2.4bdfa218.chunk.js"
+  },
+  {
+    "revision": "df0019377c993e5f79f9",
+    "url": "/malls-of-cairo/static/js/main.2d301fe9.chunk.js"
+  },
+  {
+    "revision": "34d125c21f30bce6b32b",
+    "url": "/malls-of-cairo/static/js/runtime~main.3c7dd384.js"
+  }
+]);
