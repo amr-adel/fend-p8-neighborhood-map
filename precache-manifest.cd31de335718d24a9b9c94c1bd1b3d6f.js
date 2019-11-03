3fdb0beeb39bb6a75d08258ee1f2cad6e35647db
Amr A. Abdelmoez <amradel88@live.com>
2019-11-04 00:18:24 +0200

    Updates

---
+++ precache-manifest.cd31de335718d24a9b9c94c1bd1b3d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09c6cef7e15f6ac39718ce0789a98e0",
+    "url": "/malls-of-cairo/index.html"
+  },
+  {
+    "revision": "d1930422d8712244ab0e",
+    "url": "/malls-of-cairo/static/css/main.be92bb85.chunk.css"
+  },
+  {
+    "revision": "e0b977d75cf44a177ff4",
+    "url": "/malls-of-cairo/static/js/2.2e442671.chunk.js"
+  },
+  {
+    "revision": "d1930422d8712244ab0e",
+    "url": "/malls-of-cairo/static/js/main.39a29ca0.chunk.js"
+  },
+  {
+    "revision": "c1ecfd5c6a48e1b93ae9",
+    "url": "/malls-of-cairo/static/js/runtime-main.096cc336.js"
+  },
+  {
+    "revision": "69fea0a09ed1e6a4f0e4a13fea77a34d",
+    "url": "/malls-of-cairo/static/media/fs_fallback.69fea0a0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/malls-of-cairo/precache-manifest.734e85020609485c2db1c03aaea03535.js"
+  "/malls-of-cairo/precache-manifest.cd31de335718d24a9b9c94c1bd1b3d6f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/malls-of-cairo/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
